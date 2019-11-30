993015ec926de7a32303c4df5062c0d966e71564
Jerome Nelson <jerome.nelson@betssongroup.com>
2019-11-30 21:12:10 +0100

    refactor(max chars): add deecription and extra assertion

--- max-chars/index.js
+++ max-chars/index.js
@@ -1,3 +1,7 @@
+// Solution 1 - Split string and reduce into a single option
+// by comparing the previous and the current option
+// Once the previous option is bigger return this one instead as
+// the accumulative value
 function maxChars(str) {
     const list = {};
     return str.split("").reduce((prev, curr, i) => {
--- max-chars/index.spec.js
+++ max-chars/index.spec.js
@@ -7,6 +7,7 @@ describe("maxChars", () => {
 
     it("should return the characters with the highest count", () => {
         expect(maxChars("2213aaaaaaa2")).toBe("a");
+        expect(maxChars("2213aaaa2aasda21asdasd3asdd2")).toBe("a");
     });
 });
 
