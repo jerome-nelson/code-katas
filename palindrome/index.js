07c5a3f9dc36709850e4ebc091fff37b599e3aad
Jerome Nelson <jerome.nelson@betssongroup.com>
2019-11-30 16:33:34 +0100

    refactor(palindrome): optimise solution (reduction of iterations)

--- palindrome/index.js
+++ palindrome/index.js
@@ -1,8 +1,11 @@
 // Solution 1 - Compare from the beginning and end
-function _palindrome(str) {
+
+// optimisation: We only iterate up till the middle - since I am comparing opposite ends
+// there is no need to compare once we reach the middle
+function palindrome(str) {
     const normStr = str.toLowerCase();
     return normStr.toLowerCase().split("").reduce((result, char, index, origin) => {
-            if (result) {
+            if (result || index === Math.floor(origin.length / 2)) {
                 result = char === normStr[origin.length - (index+1)];
             }
             return result;
@@ -14,7 +17,7 @@ function _palindrome(str) {
 const reverse = require("../string-reversal");
 
 // Solution 2 - Check if reversed string is the same
-function palindrome(str) {
+function _palindrome(str) {
     str = str.toLowerCase();
     return str === reverse(str, { whitespace_preserve: true });
 }
