cd8c47a65cd15ba73f1c9e3f3de00c8b0128521c
Jerome Nelson <jerome.nelson@betssongroup.com>
2019-12-22 19:31:28 +0100

    feat(pyramid): recursive solution

--- pyramid/index.js
+++ pyramid/index.js
@@ -1,5 +1,5 @@
-// Solution 1
-function pyramid(n) {
+// Solution 1 - Regular double loop
+function pyramid3(n) {
 
   // Total Amount of Columns
   const total_width = n * 2 - 1;
@@ -27,6 +27,56 @@ function pyramid(n) {
 
     console.log(columns.join(""));
   }
+}
+
+// Solution 2 - Recursive Solution
+function pyramid2(n, current_row = 1) {
+
+  // Total Amount of Columns
+  const total_width = n * 2 - 1;
+
+  // Hashes to Add
+  let toAdd = current_row === 1 ? current_row
+  : Math.round(current_row * 1.5);
+
+  if (current_row === n) {
+    console.log(`#`.repeat(total_width));
+    return;
+  }
+
+  const space = Math.floor(total_width - toAdd)/2;
+  const final = `#`.repeat(toAdd);
+
+  console.log(
+    final
+      .padStart(space+final.length)
+      .padEnd(space*2+final.length)
+  );
+  pyramid(n, current_row + 1);
+}
+
+// Alternative Recursive Solution - not using prototype functions
+function pyramid(n, row = 0, level = '') {
+
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const middle = Math.floor((2*n-1)/2)
+
+  if (middle - row <= level.length && middle + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+
+
+  pyramid(n, row, level + add);
 }
 
 module.exports = pyramid;
